8806348a2abfefd9f80eb70bcc4a3302c66a2ce7
HLIB SIN <sinixyowo@gmail.com>
2023-11-12 20:24:08 +0200

    dockerized

--- MainService/backend/resources/__init__.py
+++
@@ -1,6 +0,0 @@
-from .category import CategoryResource
-from .game import GameResource
-from .product import ProductResource
-from .review import ReviewResource
-from .cart import CartResource
-from .user import UserResource
